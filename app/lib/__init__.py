b20c2630ac6af80b90b3e10c0ea55ce305b2cc04
Luka Simić <git@kocka.tech>
2023-05-21 19:45:33 +0200

    Additional enums and typings in lib.

--- app/lib/__init__.py
+++ app/lib/__init__.py
@@ -1,5 +1,16 @@
 from enum import Enum
+from typing import Tuple, Union
+from Crypto.PublicKey import DSA, RSA
+from .elgamal import ElGamalKey
+
+Key = Union[RSA.RsaKey, Tuple[DSA.DsaKey, ElGamalKey]]
+SigningKey = Union[RSA.RsaKey, DSA.DsaKey]
+EncryptionKey = Union[RSA.RsaKey, ElGamalKey]
 
 class KeyAlgorithms(Enum):
     RSA = 1
     DSAElGamal = 2
+
+class Cipher(Enum):
+    AES128 = 1
+    TripleDES = 2
