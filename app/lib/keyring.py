9541c8fe998bd21edaed804c4237e5d10c96a82a
Katarina Jocić <katarinajocic00@gmail.com>
2023-05-26 21:26:13 +0200

    update gitignore

--- app/lib/keyring.py
+++ app/lib/keyring.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-databaseUrl = 'sqlite:///keyring2.db'
+databaseUrl = 'sqlite:///keyring.db'
 engine = create_engine(databaseUrl)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
